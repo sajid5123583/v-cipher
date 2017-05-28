e6e6dbc24082af3ea8af1e8ff220ff14a8f676d9
Sajid Ibne Anower <sajidanower23@gmail.com>
2017-05-28 14:44:13 +1000

    Printing the number of matches

--- cipher.py
+++ cipher.py
@@ -4,17 +4,21 @@
 def get_length(cipherText):
 
 	print "The length of the cipher text is " + str(len(cipherText))
-	shift_compare(cipherText, 1)
+	for shift in range(1, len(cipherText)):
+		print shift_compare(cipherText, shift)
 
 
 # Compares the cipherText with a version of it shifted by shiftLength
 def shift_compare(cipherText, shiftLength):
 	assert(shiftLength <= len(cipherText))
+	numMatch = 0
 	totalLength = len(cipherText)
 	for shiftIndex in range(shiftLength, totalLength):
 		if(cipherText[shiftIndex] == cipherText[shiftIndex - shiftLength]):
-			print "{} and {} match".format(cipherText[shiftIndex], cipherText[shiftIndex - shiftLength])
+			# print "{} and {} match".format(cipherText[shiftIndex], cipherText[shiftIndex - shiftLength])
+			numMatch += 1
 
+	return numMatch
 
 cipherText = raw_input("Enter the cipher text: ")
 get_length(cipherText)

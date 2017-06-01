0895f7361128e6c1a554f2d561a33f5590df54fb
Sajid Ibne Anower <sajidanower23@gmail.com>
2017-06-01 23:52:03 +1000

    Wrote a function to get the standard deviation of a list

--- cipher.py
+++ cipher.py
@@ -1,6 +1,30 @@
 #Function names are in underscore naming
 #function arguments are in camelCase
 
+def square(num):
+	return num ** 2
+
+def sqrt(num):
+	return num ** (1/2.0)
+
+def calc_std_dev(list):
+	mean = calc_mean(list)
+	diffList = diff_with_mean(list, mean)
+	return sqrt(sum(diffList) / float(len(diffList)))
+
+def diff_with_mean(list, mean):
+	diffList = []
+	for num in list:
+		diff = square(num - mean)
+		diffList.append(diff)
+
+	return diffList
+
+def calc_mean(list):
+	if len(list) == 0:
+		return 0
+	return sum(list) / float(len(list))
+
 def get_length(cipherText):
 
 	print "The length of the cipher text is " + str(len(cipherText))
@@ -21,7 +45,9 @@ def shift_compare(cipherText, shiftLength):
 	return numMatch
 
 cipherText = raw_input("Enter the cipher text: ")
-get_length(cipherText)
+numbers = map(int, cipherText.split())
+print str(calc_std_dev(cipherText))
+# get_length(cipherText)
 
 
 # 0 1 2 3 4 5 len 
